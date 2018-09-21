8c7793a775952c0aa5c0dfcf5591897dc770e59e
Vinny <Vinny@DESKTOP-6P3G6LF>
2018-09-21 17:49:17 -0300

    Adicionando tela iniciar,Produto,Funcionario

---
+++ src/Model/GestaoFornecedor.java
@@ -0,0 +1,15 @@
+
+package model;
+public class GestaoFornecedor {
+    String nome;
+    String cnpj;
+    String endereco;
+    boolean recorrente;
+    
+    
+    //aplicavel no preco de custo de todos os produtos por eles fornecidos.
+    public float recorrenteNegociarDesconto(float precoCusto, float valorNegociado){
+        float valorDesconto = precoCusto * valorNegociado;
+        return valorDesconto;
+    }
+}
