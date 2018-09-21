3749d7aac1d3ab42472a638c66b786ee09a5adc3
Kaio Lino <kaiomudkt@gmai.com>
2018-09-21 15:46:38 -0300

    Primeiro commit, criando classes do pacote modelo

---
+++ PadarisTremaoP1/src/Model/GestaoFuncionario.java
@@ -0,0 +1,12 @@
+package model;
+public abstract class GestaoFuncionario {
+    String nome;
+    String endereco;
+    String cpf;
+    int telefone;
+    float salarioBaseMensal;
+    
+    // sera usado nas clases vendedor, gerente e padeiro
+    public abstract int gratificacao();
+}
+
