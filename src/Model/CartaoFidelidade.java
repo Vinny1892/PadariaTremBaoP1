a5cc90c2a09b86a628660015b2a18ed907667805
Vinny <vinnyaoe@gmail.com>
2018-10-11 17:57:45 -0300

    Consertando merdas

--- src/Model/CartaoFidelidade.java
+++ src/Model/CartaoFidelidade.java
@@ -10,5 +10,31 @@ package Model;
  * @author Enoque
  */
 public class CartaoFidelidade {
+    private  boolean gold;
+    private boolean platinum;
+
+    public CartaoFidelidade(boolean gold, boolean platinum) {
+        this.gold = gold;
+        this.platinum = platinum;
+    }
+
+    public boolean isGold() {
+        return gold;
+    }
+
+    public void setGold(boolean gold) {
+        this.gold = gold;
+    }
+
+    public boolean isPlatinum() {
+        return platinum;
+    }
+
+    public void setPlatinum(boolean platinum) {
+        this.platinum = platinum;
+    }
+    
+    
+    
     
 }
