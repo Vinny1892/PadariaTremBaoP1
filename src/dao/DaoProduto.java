2196cf01fca65a5e42718d5044927f497f7e25cc
Kaio Lino <kaiomudkt@gmai.com>
2018-10-11 19:56:27 -0300

    criando classes dao

--- src/dao/DaoProduto.java
+++ src/dao/DaoProduto.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 /**
- *
- * @author Kaio
+ * na entidade PRODUTO, podemos declarar produtos
+ * mas nao sua data de validade e a quantidade,
+ * a entidade produto funciona mais como um catalago produtos
  */
 public class DaoProduto {
     
