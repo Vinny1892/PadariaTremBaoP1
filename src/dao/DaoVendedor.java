18ed60809efa4e3e88f0d5b3fab4de86923a8b02
kaio <kaiomudkt@gmail.com>
2018-10-15 20:44:01 -0400

    arrumando metodo findByName

--- src/dao/DaoVendedor.java
+++ src/dao/DaoVendedor.java
@@ -20,7 +20,7 @@ public class DaoVendedor {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public Cadastro findByName(String nome) {
+    public void findByName(String nome) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public void alterar(Cadastro cadastro) {
