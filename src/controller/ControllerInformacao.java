9af1662cbfc2a97ed8a2ccd1955e23b4a437c6e6
Mateus Ragazzi <mateusragazzi.b@gmail.com>
2018-10-22 11:48:34 -0400

    Controller de informação atualizada

--- src/controller/ControllerInformacao.java
+++ src/controller/ControllerInformacao.java
@@ -9,48 +9,130 @@ import dao.DaoProduto;
 import dao.DaoVenda;
 import dao.DaoVendedor;
 import dao.GenericDao;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.GestaoInformacao;
 
-public class ControllerInformacao {
+public final class ControllerInformacao {
 
     public ControllerInformacao(String type, String location) {
-        
+        List<Object> objects = convertType(type);
     }
 
     public ControllerInformacao(String type, String location, int cod) {
+       Object object = convertType(type, cod);
     }
 
     //nao sei se retorna uma dao ou um model...
-    GenericDao convertType(String option) {
-        GenericDao myClass;
+    protected List<Object> convertType(String option) {
         switch (option) {
             case "Fornecedores":
-                myClass = new DaoFornecedor();
-                break;
+                try {
+                    return new DaoFornecedor().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Produtos":
-                myClass = new DaoProduto();
-                break;
+                try {
+                    return new DaoProduto().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Padeiros":
-                myClass = new DaoPadeiro();
-                break;
+                try {
+                    return new DaoPadeiro().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Vendedores":
-                myClass = new DaoVendedor();
-                break;
+                try {
+                    return new DaoVendedor().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Gerentes":
-                myClass = new DaoGerente();
-                break;
+                try {
+                    return new DaoGerente().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Clientes":
-                myClass = new DaoCliente();
-                break;
+                try {
+                    return new DaoCliente().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Venda":
-                myClass = new DaoVenda();
-                break;
+                try {
+                    return new DaoVenda().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             case "Estoque":
-                myClass = new DaoEstoque();
-                break;
+                try {
+                    return new DaoEstoque().getAll();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
             default:
-                myClass = null;
+                return null;
+        }        
+    }
+
+    private Object convertType(String type, int cod) {
+        switch (type) {
+            case "Fornecedores":
+                try {
+                    return new DaoFornecedor().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Produtos":
+                try {
+                    return new DaoProduto().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Padeiros":
+                try {
+                    return new DaoPadeiro().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Vendedores":
+                try {
+                    return new DaoVendedor().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Gerentes":
+                try {
+                    return new DaoGerente().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Clientes":
+                try {
+                    return new DaoCliente().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Venda":
+                try {
+                    return new DaoVenda().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            case "Estoque":
+                try {
+                    return new DaoEstoque().getById(cod);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerInformacao.class.getName()).log(Level.SEVERE, null, ex);
+                }
         }
-        return myClass;
+        return null;
     }
 }
